49d471e6ddcf2293faa806e57b845a9612f17fcc
Marcus Sena <mens.eai20@uea.edu.br>
2023-09-20 06:30:17 -0400

    Problem 1.2 Check Permutation #Second Solution
    Second Solution Implemented

--- Cap1Problem2.kt
+++ Cap1Problem2.kt
@@ -2,6 +2,7 @@ import com.sun.jdi.Value
 
 // Given 2 strings, write a method to decide if one is a permutation of the other
 
+//Make 2 CharArrays, Sort them and Compare
 fun isPermutationBF(string1: String, string2: String){
     if (string1.length != string2.length){
         return println("Não é anagrama")
@@ -18,10 +19,35 @@ fun isPermutationBF(string1: String, string2: String){
     return println("Não é anagrama")
 }
 
+//Make an Int Array of the ASCII, Make a CharArray of one of the Arrays, Update the IntArray with info of the CharArray and then Compare
+fun isPermutationCount(string1: String, string2: String){
+    if (string1.length != string2.length){
+        return println("Não é anagrama")
+    }
+    var letters: Array<Int> = Array(128) {0}
+    val string1CharArray = string1.toCharArray()
+
+    string1CharArray.forEach { char ->
+        letters[char.code]++
+    }
+
+    string2.forEach {char->
+        letters[char.code]--
+        if (letters[char.code] < 0){
+            return println("Não é anagrama")
+        }
+    }
+    return println("É anagrama")
+
+}
+
 
 fun main(){
     val string1: String = "abcdez"
-    val string2: String = "zedcba"
+    val string2: String = "zedcb"
+    println("Solution 1")
     isPermutationBF(string1, string2)
+    println("Solution 2")
+    isPermutationCount(string1,string2)
 
 }
